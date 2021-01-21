aed8dcc49f1ee3ddb9db2a629292f5a6128b761a
Manohar2710 <manohar1ga15ec430@gmail.com>
2021-01-21 18:19:05 +0530

    Implemented Notification App

---
+++ AndvanceAndroidCourse/app/src/main/java/com/appscrip/andvanceandroid/receiver/SnoozeReceiver.kt
@@ -0,0 +1,13 @@
+package com.appscrip.andvanceandroid.receiver
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class SnoozeReceiver : BroadcastReceiver() {
+
+	override fun onReceive(context: Context, intent: Intent) {
+		// This method is called when the BroadcastReceiver is receiving an Intent broadcast.
+		TODO("SnoozeReceiver.onReceive() is not implemented")
+	}
+}
